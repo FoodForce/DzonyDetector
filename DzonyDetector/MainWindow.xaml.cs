22532e13ed9eb03552a015f5d51a7cfb4f12753f
Sebera <sebera@techsys.cz>
2021-08-06 15:59:49 +0200

    Add project files.

---
+++ DzonyDetector/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DzonyDetector
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly SolidColorBrush strokeBrush = new(Colors.Red);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            foreach (var prevRect in Faces)
+                canvas.Children.Remove(prevRect);
+            Faces.Clear();
+
+            string[] filesPaths = (string[])e.Data.GetData(DataFormats.FileDrop);
+            // TODO: odlisit obraz, vice obrazu, slozku a vice slozek
+            im.Source = new BitmapImage(new Uri(filesPaths[0]));
+            
+            System.Drawing.Rectangle[] faces = FaceDetector.FaceDetector.Detect(filesPaths[0]);
+            foreach(var face in faces)
+            {
+                Rectangle faceRect = new()
+                {
+                    Width = face.Width,
+                    Height = face.Height,
+                    StrokeThickness = 5,
+                    Stroke = strokeBrush
+                };
+                Canvas.SetLeft(faceRect, face.X);
+                Canvas.SetTop(faceRect, face.Y);
+                canvas.Children.Add(faceRect);
+                Faces.Add(faceRect);
+            }
+        }
+
+        public List<Rectangle> Faces { get; } = new();
+    }
+}
---
+++ FaceDetector/FaceDetector.cs
@@ -0,0 +1,20 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Drawing;
+
+namespace FaceDetector
+{
+    public static class FaceDetector
+    {
+        //static readonly CascadeClassifier classifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+        static readonly CascadeClassifier classifier = new CascadeClassifier("haarcascade_frontalface_alt.xml");
+
+        public static Rectangle[] Detect(string imageFilePath)
+        {
+            Image<Rgb, byte> im = new(imageFilePath);
+            Rectangle[] faces = classifier.DetectMultiScale(im, 1.4, 1);
+            return faces;
+        }
+    }
+}
